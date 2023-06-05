f098d451a9f5e78a70420a13164c1f1851a37bc2
CanaryXDAO <95591343+CanaryXDAO@users.noreply.github.com>
2023-06-05 11:58:04 -0400

    2

--- functions/walletverification.js
+++ functions/walletverification.js
@@ -1,27 +1,30 @@
 const Web3 = require("web3");
 
-
-const web3 = new Web3(); // No need to connect to a provider here.
+const web3 = new Web3();
 
 exports.handler = async function (event, context) {
   try {
     const { signature, message, account } = JSON.parse(event.body);
 
-    // Recover the account address from the message and signature
     const messageHash = web3.utils.sha3(web3.utils.fromUtf8(message));
     const recoveredAccount = web3.eth.accounts.recover(messageHash, signature);
 
-    // Check if the recovered account matches the original account
     if (recoveredAccount.toLowerCase() === account.toLowerCase()) {
-      // The signature is valid; proceed with your logic.
       return {
         statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({ success: true }),
       };
     } else {
-      // The signature is invalid
       return {
         statusCode: 401,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({ success: false, error: "Invalid signature" }),
       };
     }
@@ -29,6 +32,10 @@ exports.handler = async function (event, context) {
     console.error(error);
     return {
       statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({ success: false, error: "Internal server error" }),
     };
   }
